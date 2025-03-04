bf77eb3f7cada1b1d12b02284f05026992a4621b
arancksj22 <arancksj@gmail.com>
2025-03-05 01:34:53 +0530

    index

--- Frontend/src/components/IndexTitleSliding.jsx
+++ Frontend/src/components/IndexTitleSliding.jsx
@@ -24,18 +24,13 @@ const IndexTitleSliding = () => {
 
     return (
         <div id="Index-Features">
+            <div id="IndexTitleOpener">Real AI</div>
             <div className="Index-FeatureContainer">
-                {/* Left Side Features */}
-                <div id="IndexTitleOpener">
-                Real AI
-                </div>
                 <div id="Index-feature1" className="Index-cardL" ref={(el) => (cardsRef.current[1] = el)}>
                     <div className="Index-cardtitle">Smart Business Strategy</div>
                     <div className="Index-cardtext">Leverage AI-generated strategic advice tailored to your industry, helping you navigate product development, business models, and growth strategies.</div>
                 </div>
             </div>
-
-            <br /><br />
         </div>
     );
 };
